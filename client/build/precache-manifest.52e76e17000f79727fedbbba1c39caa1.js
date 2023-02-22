dfcc60b0e464601759f87c92f81edb7780d4b89b
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-02-22 17:47:14 -0500

    changes

---
+++ client/build/precache-manifest.52e76e17000f79727fedbbba1c39caa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f80e1efe190a43aa089003213961d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3477f082765eaf93cda6",
+    "url": "/static/css/main.be2b856d.chunk.css"
+  },
+  {
+    "revision": "3a38aff792b698f4c261",
+    "url": "/static/js/2.a4695085.chunk.js"
+  },
+  {
+    "revision": "6b4c7bad46f8f7931a3a3a16e9f9f929",
+    "url": "/static/js/2.a4695085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3477f082765eaf93cda6",
+    "url": "/static/js/main.0a0292d4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0a0292d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
