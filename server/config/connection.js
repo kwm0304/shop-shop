a7ec04df8e292f64c791a74073e69f2588915fbd
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-02-22 18:53:49 -0500

    changed connection uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-shopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+mongoose.connection.on('connected', () =>
+console.log('Connected to mongodb endpoint'));
 
 module.exports = mongoose.connection;
