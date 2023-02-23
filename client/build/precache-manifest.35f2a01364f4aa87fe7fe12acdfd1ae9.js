d9524a746cd6119f3add178a85142e42e1d76b04
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-02-22 23:58:38 -0500

    trying again after redoing build

---
+++ client/build/precache-manifest.35f2a01364f4aa87fe7fe12acdfd1ae9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e5e3d645e81f115ff40db94380a825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4b471ff944b8449e5c2",
+    "url": "/static/css/main.be2b856d.chunk.css"
+  },
+  {
+    "revision": "3a38aff792b698f4c261",
+    "url": "/static/js/2.a4695085.chunk.js"
+  },
+  {
+    "revision": "6b4c7bad46f8f7931a3a3a16e9f9f929",
+    "url": "/static/js/2.a4695085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b471ff944b8449e5c2",
+    "url": "/static/js/main.f123556c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f123556c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
