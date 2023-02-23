b7b574cf02f0f430977caee39b59a20f8dc5a283
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-02-22 19:01:49 -0500

    changed mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-shopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 mongoose.connection.on('connected', () =>
 console.log('Connected to mongodb endpoint'));
