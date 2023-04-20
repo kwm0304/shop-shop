f7a70c74b22c7bce3c69af12eb754829d962eb84
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-04-20 15:25:05 -0400

    required dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1.27017/mern-shopping', {
+mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useUnifiedTopology: true,
   useCreateIndex: true
 });
 
